bd08ffdf6494430b8b245d46350cfcd372c664ff
andrewjordan3 <andrewjordan3@gmail.com>
2025-11-20 14:16:03 -0600

    Added the get card summary operation.

--- src/fuelsync/utils/__init__.py
+++ src/fuelsync/utils/__init__.py
@@ -4,7 +4,7 @@ from .config_loader import FuelSyncConfig, load_config
 from .datetime_utils import format_for_soap
 from .logger import setup_logger
 from .login import login_to_efs
-from .model_tools import is_nil, safe_convert
+from .model_tools import is_nil, parse_xml_to_dict
 from .xml_parser import check_for_soap_fault, extract_soap_body, parse_soap_response
 
 __all__: list[str] = [
@@ -21,7 +21,7 @@ __all__: list[str] = [
     'login_to_efs',
     # xml_parser.py
     'parse_soap_response',
-    'safe_convert',
+    'parse_xml_to_dict',
     # logger.py
     'setup_logger',
 ]
