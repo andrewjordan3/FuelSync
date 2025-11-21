56722862db5c2363d0314b3c36fd305c5767355c
andrewjordan3 <andrewjordan3@gmail.com>
2025-11-21 09:41:56 -0600

    Added pipeline to the main package init.

--- src/fuelsync/__init__.py
+++ src/fuelsync/__init__.py
@@ -1,8 +1,11 @@
 # fuelsync/__init__.py
 
 from .efs_client import EfsClient
+from .pipeline import FuelPipeline
 
 __all__: list[str] = [
     # efs_client.py
-    'EfsClient'
+    'EfsClient',
+    # pipeline.py
+    'FuelPipeline',
 ]
